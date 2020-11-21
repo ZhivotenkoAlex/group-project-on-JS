009ef7dc88d23ce8011e550fa0ab56f73c33da54
Ivan <vitanlhouse@gmail.com>
2020-11-21 16:20:53 +0200

    Додає відкривання фільма у модалці

---
+++ src/js/modal-movie-card.js
@@ -0,0 +1,27 @@
+import '../../node_modules/basiclightbox/dist/basicLightbox.min.css';
+import * as basicLightbox from 'basiclightbox';
+
+import './get-refs';
+import refs from './get-refs';
+import cardMovieTemplate from '../templates/modal-tmp.hbs';
+
+refs.filmContainer.addEventListener('click', showMovieCard);
+
+async function showMovieCard(event) {
+  basicLightbox
+    .create(
+      cardMovieTemplate(
+        await fetchMovie(
+          event.target.closest('.movies-item').getAttribute('id'),
+        ),
+      ),
+    )
+    .show();
+}
+
+async function fetchMovie(id) {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=7e78d9d0b80a5a9938ce5aba09bf2c47`,
+  );
+  return await response.json();
+}
