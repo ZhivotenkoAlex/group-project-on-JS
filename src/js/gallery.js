dba63eef5e712f0e826d1899c754fe9f958ce33a
Zhivotenko Aleksandr <zhivotenko.a.s@gmail.com>
2020-11-23 01:20:20 +0200

    1

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,95 +1,44 @@
-import galleryItems from './gallery-items.js';
+// const galleryContainer = document.querySelector(".team-link");
+// const modal = document.querySelector(".js-lightbox");
+// const modalImg = document.querySelector(".lightbox__image");
+// const modalContent = document.querySelector(".lightbox__image");
+// const overlay = document.querySelector(".lightbox__overlay")
 
-const galleryContainer = document.querySelector(".team-link");
-const modal = document.querySelector(".js-lightbox");
-const modalImg = document.querySelector(".lightbox__image");
-const modalContent = document.querySelector(".lightbox__image");
-const overlay = document.querySelector(".lightbox__overlay")
-const modalBtnClose = document.querySelector(".lightbox__button");
-const modalBtnRight = document.querySelector(".scroll-right");
-const modalBtnLeft = document.querySelector(".scroll-left");
-
-
+const openTeamModalBtn = document.querySelector('.js-open-modal');
+const closeTeamModalBtn = document.querySelector('.js-team-close');
+const backdropTeamModal = document.querySelector('.js-backdrop');
+const body = document.querySelector('body');
 
 galleryContainer.addEventListener('click', modalOpen);
+openTeamModalBtn.addEventListener('click', onOpenModal);
 
-console.log(galleryContainer);
+function onOpenModal(evt) {
+    evt.preventDefault();
 
-function modalOpen(event) {
-    event.preventDefault();
+    window.addEventListener('keydown', onEscKeyPress);
+    closeTeamModalBtn.addEventListener('click', onCloseModal);
+    backdropTeamModal.addEventListener('click', onBackdropClick);
+    body.classList.add('popup-open');
+    body.classList.add('show-modal');
+}
 
-    modal.classList.add("is-open");
-    modalImg.src = event.target.dataset.source;
-    modalImg.alt = event.target.alt;
+function onCloseModal() {
 
-    overlay.addEventListener("click", modalCloseByOverlayClick);
-    document.addEventListener("keydown", modalCloseByEsc);
-    modalBtnClose.addEventListener('click', modalClose);
-    window.addEventListener("keydown", modalImgScrolling);
-    modalBtnRight.addEventListener("click", modalImgScrolling);
-    modalBtnLeft.addEventListener("click", modalImgScrolling);
-    modalContent.addEventListener("click", modalImgScrolling);
-};
-galleryContainer.addEventListener('click', modalOpen);
+    document.body.classList.remove('show-modal');
+    document.body.classList.remove('popup-open');
+    closeTeamModalBtn.removeEventListener('click', onCloseModal);
+    backdropTeamModal.removeEventListener('click', onBackdropClick);
+}
 
-// закрытие по иконке
-
-function modalClose(event) {
-    modal.classList.remove("is-open");
-    modalBtnClose.removeEventListener('click', modalClose);
-    window.removeEventListener("keydown", modalImgScrolling);
-    modalBtnRight.removeEventListener("click", modalImgScrolling);
-    modalBtnLeft.removeEventListener("click", modalImgScrolling);
-    modalContent.removeEventListener("click", modalImgScrolling);
-};
-
-// закрытие по эскейпу
-
-function modalCloseByEsc(event) {
-    if (event.code === "Escape") {
-        modalClose(event)
-    }
-    document.removeEventListener("keydown", modalCloseByEsc);
-    window.removeEventListener("keydown", modalImgScrolling);
-    modalBtnRight.removeEventListener("click", modalImgScrolling);
-    modalBtnLeft.removeEventListener("click", modalImgScrolling);
-    modalContent.removeEventListener("click", modalImgScrolling);
-};
-
-// закрытие по клику вне модалки
-
-function modalCloseByOverlayClick(event) {
+function onBackdropClick(event) {
+    console.log(event, event.currentTarget, event.target);
     if (event.currentTarget === event.target) {
-        modalClose(event)
+        onCloseModal();
     }
-    overlay.removeEventListener("click", modalCloseByOverlayClick);
-    window.removeEventListener("keydown", modalImgScrolling);
-    modalBtnRight.removeEventListener("click", modalImgScrolling);
-    modalBtnLeft.removeEventListener("click", modalImgScrolling);
-    modalContent.removeEventListener("click", modalImgScrollinremove);
-};
+}
 
-// бесконечное пролистывание
-
-function modalImgScrolling(event) {
-
-    let imgIndex = galleryItems.findIndex(img => img.original === modalImg.src);
-
-    if (event.code === 'ArrowLeft' || event.code === 'ArrowDown' || modalBtnLeft === event.target) {
-        if (imgIndex === 0) {
-            imgIndex += galleryItems.length;
-        }
-        imgIndex -= 1;
-    };
-
-    if (event.code === 'ArrowRight' || event.code === 'ArrowUp' || modalBtnRight === event.target || modalContent === event.target) {
-        if (imgIndex === galleryItems.length - 1) {
-            imgIndex -= galleryItems.length;
-        }
-        imgIndex += 1;
-    };
-
-    modalImg.src = galleryItems[imgIndex].original;
-    modalImg.alt = galleryItems[imgIndex].description;
-
-};
+function onEscKeyPress(event) {
+    if (event.code === 'Escape') {
+        onCloseModal();
+    }
+}
